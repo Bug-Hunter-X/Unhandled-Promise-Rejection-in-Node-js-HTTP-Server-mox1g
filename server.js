24901a4f598cf3707060804c73ffdd34954971f2
BugHunter-X <askzenai@gmail.com>
2025-01-28 04:16:07 +0100

    Add server.js file

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({ message: 'Hello from Node.js' }));
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
+
+//Unhandled promise rejection error
+//This error occurs when a promise is rejected and there is no error handler to catch it
